cdf5aca38842edae9e4f5aacef670968db574a6e
Marius Demeter <marius.demeter96@yahoo.com>
2023-06-28 08:59:51 +0300

    Added solution for Assignment 4

--- shared.js
+++ shared.js
@@ -1,11 +1,17 @@
-var backdrop = document.querySelector('.backdrop');
-var modal = document.querySelector('.modal');
-var selectPlanButtons = document.querySelectorAll('.plan button');
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var selectPlanButtons = document.querySelectorAll(".plan button");
 
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
-    selectPlanButtons[i].addEventListener('click', function() {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
-    });
-}   
+  selectPlanButtons[i].addEventListener("click", function () {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
+
+var modalActionButtonNo = document.querySelector(".modal__actions button");
+modalActionButtonNo.addEventListener("click", function () {
+  modal.style.display = "none";
+  backdrop.style.display = "none";
+});
